3eb9de98f5c1ef4c9eaaad45fd8a5db3f97e6559
d.boldyrev <dmitry.sieg@gmail.com>
2018-01-22 17:40:45 +0200

    Refactored and extended blur generator

--- js/FilteringGenerator.js
+++ js/FilteringGenerator.js
@@ -6,36 +6,62 @@ define(function() {
 		this.blur = blur;
 		this.map = [];
 		this.tmpmap = [];
-		// generate
+		// arrays init
 		for (var j = 0; j < height; j++) {
 		    this.map[j] = [];
 		    this.tmpmap[j] = [];
-		    for (var i = 0; i < width; i++) {
-		        this.map[j][i] = 1 + this.getRandom(this.depth);
-		        this.tmpmap[j][i] = this.map[j][i];
-		    }
 		}
-
-		// blur
-		for (var j = 1; j < height - 1; j++) {
-            for (var i = 1; i < width - 1; i++) {
-                var sum =
-                    this.map[j - 1][i] +
-                    this.map[j + 1][i] +
-                    this.map[j][i - 1] +
-                    this.map[j][i + 1];
-                this.tmpmap[j][i] = Math.floor(sum / 4);
-            }
-        }
-        for (var j = 1; j < height - 1; j++) {
-            for (var i = 1; i < width - 1; i++) {
-                this.map[j][i] = this.tmpmap[j][i];
-            }
-        }
 	};
 
 	FilteringGenerator.prototype = {
 
+        cycle: function(offset, action) {
+            for (var j = offset; j < this.height - offset; j++) {
+                for (var i = offset; i < this.width - offset; i++) {
+                    action.call(this, i, j);
+                }
+            }
+        },
+        generate: function() {
+
+            // randomize
+            this.cycle(0, function(i, j) {
+                this.map[j][i] = 1 + this.getRandom(this.depth);
+                this.tmpmap[j][i] = this.map[j][i];
+            });
+
+            // blur
+            for (var b = 0; b < this.blur; b++) {
+                this.cycle(1, function(i, j) {
+                    this.tmpmap[j][i] = this.mask2(i, j);
+                });
+                this.cycle(1, function(i, j) {
+                    this.map[j][i] = this.tmpmap[j][i];
+                });
+            }
+
+            return this;
+        },
+        mask: function(i, j) {
+            var sum =
+                this.map[j - 1][i] +
+                this.map[j + 1][i] +
+                this.map[j][i - 1] +
+                this.map[j][i + 1];
+            return Math.floor(sum / 4);
+        },
+        mask2: function(i, j) {
+            var sum =
+                this.map[j - 1][i - 1] +
+                this.map[j    ][i - 1] +
+                this.map[j + 1][i - 1] +
+                this.map[j - 1][i    ] +
+                this.map[j + 1][i    ] +
+                this.map[j - 1][i + 1] +
+                this.map[j    ][i + 1] +
+                this.map[j + 1][i + 1];
+            return Math.floor(sum / 8);
+        },
 		getHeight: function(x, y) {
 			return this.map[y][x];
 		},
