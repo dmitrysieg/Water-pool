a5ef88c07de6d50943e2a9b083acaea3695dd619
d.boldyrev <dmitry.sieg@gmail.com>
2018-01-24 00:02:40 +0200

    Restructured folders

--- js/app.js
+++ js/app.js
@@ -1,35 +1,46 @@
-require(['standardgenerator', 'harmonicgenerator', 'pool', 'pool3dview', 'jsongenerator', 'filteringgenerator'],
-	function(StandardGenerator, HarmonicGenerator, Pool, PoolView, JsonGenerator, FilteringGenerator) {
+require([
+    'generator/standardgenerator',
+    'generator/harmonicgenerator',
+    'generator/jsongenerator',
+    'generator/filteringgenerator',
+    'pool',
+    'pool3dview'
+], function(
+    StandardGenerator,
+    HarmonicGenerator,
+    JsonGenerator,
+    FilteringGenerator,
+    Pool,
+    PoolView
+) {
+    function createUI() {
+        var uiPanel = document.createElement("div");
+        uiPanel.id = "infopanel";
 
-	    function createUI() {
-	        var uiPanel = document.createElement("div");
-	        uiPanel.id = "infopanel";
+        var textPanel = document.createElement("p");
+        textPanel.id = "infotext";
+        uiPanel.appendChild(textPanel);
 
-	        var textPanel = document.createElement("p");
-	        textPanel.id = "infotext";
-	        uiPanel.appendChild(textPanel);
+        return uiPanel;
+    };
 
-	        return uiPanel;
-	    };
-
-		document.body.appendChild(createUI());
-		var pool = new Pool(
-			100, 100, 0,
-			/*new HarmonicGenerator(
-				16, 20, 16, {
-					n: 4,
-					x: {a: [-0.5, -0.5, -0.8, -0.5], b: [0.0, 0.0, 0.0, 0.5]},
-					y: {a: [-0.5, -0.5, -0.8, -0.5], b: [0.0, 0.0, 0.0, 0.5]},
-					randomize: false
-				}
-			)*/
-			new FilteringGenerator(100, 100, 20, 2).generate(),
-            /*new JsonGenerator("[[2,1,2,2,1,2,1,2,1,2,1,2,1,2,1,2],[2,2,2,1,2,1,2,1,2,1,1,1,2,2,2,2],[2,2,1,1,1,1,1,1,2,1,2,1,1,2,1,2],[2,2,2,2,2,2,2,2,1,1,2,2,1,1,1,2],[2,2,1,2,2,2,1,2,2,1,2,1,2,2,2,1],[1,2,2,2,1,1,1,1,2,2,1,1,2,2,1,1],[2,2,1,1,1,1,1,2,2,2,1,2,1,1,2,1],[1,1,1,2,1,1,1,2,2,1,1,2,2,1,2,2],[2,2,1,1,2,2,1,2,1,1,2,1,2,2,2,2],[1,1,1,2,2,1,1,2,1,2,2,2,2,2,1,1],[1,2,1,2,2,1,2,2,1,2,1,2,2,2,1,1],[1,1,2,1,2,2,2,2,1,2,1,1,2,2,2,1],[1,1,1,2,1,1,2,1,1,2,1,1,1,2,1,1],[1,1,2,1,1,1,1,2,1,2,2,2,1,1,1,2],[2,1,2,1,1,2,1,1,1,2,1,2,1,2,2,1],[1,1,1,2,2,1,2,2,1,1,2,2,1,2,2,2]]")*/
-		);
-		pool.fill();
-		//new PoolTableView(pool, document.getElementById('pool'), 8, 8).render();
-		var poolView = new PoolView(pool, document.body);
-		poolView.init();
-		poolView.animate();
-	}
-);
+    document.body.appendChild(createUI());
+    var pool = new Pool(
+        100, 100, 0,
+        /*new HarmonicGenerator(
+            16, 20, 16, {
+                n: 4,
+                x: {a: [-0.5, -0.5, -0.8, -0.5], b: [0.0, 0.0, 0.0, 0.5]},
+                y: {a: [-0.5, -0.5, -0.8, -0.5], b: [0.0, 0.0, 0.0, 0.5]},
+                randomize: false
+            }
+        )*/
+        new FilteringGenerator(100, 100, 20, 2).generate(),
+        /*new JsonGenerator("[[2,1,2,2,1,2,1,2,1,2,1,2,1,2,1,2],[2,2,2,1,2,1,2,1,2,1,1,1,2,2,2,2],[2,2,1,1,1,1,1,1,2,1,2,1,1,2,1,2],[2,2,2,2,2,2,2,2,1,1,2,2,1,1,1,2],[2,2,1,2,2,2,1,2,2,1,2,1,2,2,2,1],[1,2,2,2,1,1,1,1,2,2,1,1,2,2,1,1],[2,2,1,1,1,1,1,2,2,2,1,2,1,1,2,1],[1,1,1,2,1,1,1,2,2,1,1,2,2,1,2,2],[2,2,1,1,2,2,1,2,1,1,2,1,2,2,2,2],[1,1,1,2,2,1,1,2,1,2,2,2,2,2,1,1],[1,2,1,2,2,1,2,2,1,2,1,2,2,2,1,1],[1,1,2,1,2,2,2,2,1,2,1,1,2,2,2,1],[1,1,1,2,1,1,2,1,1,2,1,1,1,2,1,1],[1,1,2,1,1,1,1,2,1,2,2,2,1,1,1,2],[2,1,2,1,1,2,1,1,1,2,1,2,1,2,2,1],[1,1,1,2,2,1,2,2,1,1,2,2,1,2,2,2]]")*/
+    );
+    pool.fill();
+    //new PoolTableView(pool, document.getElementById('pool'), 8, 8).render();
+    var poolView = new PoolView(pool, document.body);
+    poolView.init();
+    poolView.animate();
+});
