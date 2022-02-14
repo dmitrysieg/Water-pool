eb3c46fd8b7a447f09416b094fb90c372979a455
Dmitry Sieg <dmitry.sieg@gmail.com>
2022-02-14 21:59:20 +0300

    Remove extra code from tests

--- src/test/java/ru/dmitry/sieg/logic/PoolSolverTest.java
+++ src/test/java/ru/dmitry/sieg/logic/PoolSolverTest.java
@@ -4,9 +4,6 @@ import org.junit.Assert;
 import org.junit.Test;
 import ru.dmitry.sieg.entity.Pool;
 import ru.dmitry.sieg.entity.PoolParams;
-import ru.dmitry.sieg.logic.generator.Generator;
-import ru.dmitry.sieg.logic.generator.GeneratorManager;
-import ru.dmitry.sieg.logic.generator.JsonGenerator;
 
 import java.util.Arrays;
 
@@ -22,16 +19,12 @@ public class PoolSolverTest {
 
         final Pool pool = new Pool(poolParams);
 
-        final Generator generator = new JsonGenerator("[" +
-                "[12, 12, 12, 12, 12]," +
-                "[12, 11, 11,  9, 12]," +
-                "[12, 12, 12, 11, 12]," +
-                "[12, 12, 12, 12, 12]" +
-                "]");
-        generator.init(poolParams);
-
-        final GeneratorManager generatorManager = new GeneratorManager();
-        generatorManager.generate(pool, generator);
+        pool.setPoolHeight(new int[][]{
+                new int[]{12, 12, 12, 12, 12},
+                new int[]{12, 11, 11,  9, 12},
+                new int[]{12, 12, 12, 11, 12},
+                new int[]{12, 12, 12, 12, 12}
+        });
 
         final PoolSolver poolSolver = new PoolSolver(pool);
         poolSolver.solve(pool);
